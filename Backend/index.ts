b58eebad1fcd8f62cae2815405b77fa64c346af8
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-28 14:29:38 -0300

    agregacion de ruta del swagger

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,5 +1,5 @@
-import app from './app';
-import dotenv from 'dotenv';
+import app from "./app";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,4 +8,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
   console.log(`Health check: http://localhost:${PORT}/health`);
+  console.log(`Swagger corriendo en: http://localhost:${PORT}/api-docs`);
 });
